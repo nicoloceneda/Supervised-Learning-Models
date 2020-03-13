9f392f509495a0cdec6cb52aae3d6f9b7f9f0b22
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-03-13 12:07:05 +0100

    Add files via upload

--- 08_multilayer_perceptron_gd.py
+++ 08_multilayer_perceptron_gd.py
@@ -51,12 +51,13 @@ class MultilayerPerceptron:
             Number of epochs.
         shuffle : bool
             If set to true it shuffles the training set before each epoch to prevent cycles.
+        l2 : float
+            Lambda parameter for L2-regularization to decrease degree of overfitting.
         n_samples_mb : int
             Number of training samples per minibatch.
-        n_units_h : int
+        n_hidden : int
             Number of units in the hidden layer.
-        l2 : float
-            Lambda value for L2-regularization.
+
 
         Attributes:
         ----------
@@ -64,14 +65,14 @@ class MultilayerPerceptron:
             Dictionary containing the cost, training accuracy and validation accuracy for each epoch during training.
     """
 
-    def __init__(self, eta=0.01, n_epochs=100, shuffle=True, n_samples_mb=1, n_units_h=30, l2=0.0):
+    def __init__(self, eta=0.01, n_epochs=100, shuffle=True, l2=0.0, n_samples_mb=1, n_hidden=30):
 
         self.eta = eta
         self.n_epochs = n_epochs
         self.shuffle = shuffle
-        self.n_samples_mb = n_samples_mb
-        self.n_units_h = n_units_h
         self.l2 = l2
+        self.n_samples_mb = n_samples_mb
+        self.n_hidden = n_hidden
 
     def one_hot_encoding(self, y_train, n_labels):
 
@@ -184,6 +185,9 @@ class MultilayerPerceptron:
             self : object
         """
 
+        # Dataset characteristics
+
+        n_samples = X_train.shape[0]
         n_features = X_train.shape[1]
         n_labels = np.unique(y_train).shape[0]
 
@@ -196,24 +200,129 @@ class MultilayerPerceptron:
         self.b_out = np.zeros(n_labels)
         self.W_out = rgen.normal(loc=0.0, scale=0.1, size=(self.n_units_h, n_labels))
 
-        # Progress formatting
+        # TODO: understand
 
         n_epochs_strlen = len(str(self.n_epochs))
         self.eval = {'cost': [], 'train_acc': [], 'valid_acc': []}
 
+
         y_train_enc = self.one_hot_encoding(y_train, n_labels)
 
+        # Iterate over each epoch and minibatch
+
+        for epoch in range(self.n_epochs):
+
+            indices = np.arange(n_samples)
+
+            if self.shuffle:
+
+                rgen.shuffle(indices)
+
+            for start_mb in range(0, n_samples - self.n_samples_mb + 1, self.n_samples_mb):
+
+                index_mb = indices[start_mb : start_mb + self.n_samples_mb]
+
+                # Forward propagate
+
+                z_h, A_h, z_out, A_out = self.forward_propagate(X_train_std[index_mb])
+
+                # Back propagate TODO: understand
+
+                delta_out = A_out - y_train_enc[index_mb]
+                sigmoid_derivative_h = A_h * (1 - A_h)
+                delta_h = np.dot(delta_out, self.W_out.T) * sigmoid_derivative_h
+
+                grad_W_h = np.dot(X_train[index_mb].T, delta_h)
+                grad_b_h = np.sum(delta_h, axis=0)
+
+                grad_W_out = np.dot(A_h.t, delta_out)
+                grad_b_out = np.sum(delta_out, axis=0)
+
+                # Regularization and weight updates
+
+                delta_W_h = grad_W_h + self.l2 * self.W_h
+                delta_b_h = grad_b_h
+                self.W_h -= self.eta * delta_W_h
+                self.b_h -= self.eta * delta_b_h
+
+                delta_W_out = grad_W_out + self.l2 * self.W_out
+                delta_b_out = grad_b_out
+                self.W_out -= self.eta * delta_W_out
+                self.b_out -= self.eta * delta_b_out
+
+            # Evaluation
+
+            z_h, a_h, z_out, a_out = self._forward(X_train)
+
+            cost = self._compute_cost(y_enc=y_train_enc,
+                                          output=a_out)
+
+            y_train_pred = self.predict(X_train)
+            y_valid_pred = self.predict(X_valid)
+
+            train_acc = ((np.sum(y_train == y_train_pred)).astype(np.float) /
+                             X_train.shape[0])
+            valid_acc = ((np.sum(y_valid == y_valid_pred)).astype(np.float) /
+                             X_valid.shape[0])
+
+            sys.stderr.write('\r%0*d/%d | Cost: %.2f '
+                                 '| Train/Valid Acc.: %.2f%%/%.2f%% ' %
+                                 (epoch_strlen, i + 1, self.epochs, cost,
+                                  train_acc * 100, valid_acc * 100))
+            sys.stderr.flush()
+
+            self.eval_['cost'].append(cost)
+            self.eval_['train_acc'].append(train_acc)
+            self.eval_['valid_acc'].append(valid_acc)
+
+        return self
 
 # -------------------------------------------------------------------------------
 # 3. TRAIN THE MULTILAYER PERCEPTRON
 # -------------------------------------------------------------------------------
 
 
-# Initiate a multilayer perceptron object
+# Initiate a multilayer perceptron object: 784 input units (n_features), 100 hidden units (n_hidden), 10 output units (n_output)
 
-mlp = MultilayerPerceptron(eta=0.0005, n_epochs=200, shuffle=True, n_samples_mb=100, n_units_h=100, l2=0.01)
+mlp = MultilayerPerceptron(eta=0.0005, n_epochs=200, shuffle=True, l2=0.01, n_samples_mb=100, n_hidden=100)
 
 
-# Learn from the data via the fit method
+# Learn from the data via the fit method: 55000 samples for training, 5000 samples for validation
 
-mlp.fit(X_train_std[:55000], y_train[:55000], X_train_std[55000:], y_train[55000:])
+mlp.fit(X_train=X_train_std[:55000], y_train=y_train[:55000], X_valid=X_train_std[55000:], y_valid=y_train[55000:])
+
+
+# -------------------------------------------------------------------------------
+# 3. MODEL EVALUATION
+# -------------------------------------------------------------------------------
+
+
+# Plot the cost function per epoch
+
+plt.figure()
+plt.plot(range(1, len(mlp.evaluation['cost'] + 1), mlp.evaluation['cost'], marker='o'))
+plt.title('Cost function per epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Cost')
+plt.savefig('images/08_multilayer_perceptron_gd/Number_of_misclassifications_per_epoch.png')
+
+
+# Plot the training and validation accuracy per epoch
+
+plt.figure()
+plt.plot(range(1, len(mlp.evaluation['train_acc'] + 1), mlp.evaluation['train_acc'], marker='o', label='train'))
+plt.plot(range(1, len(mlp.evaluation['valid_acc'] + 1), mlp.evaluation['train_acc'], marker='o', label='valid'))
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='bottom right')
+plt.savefig('images/08_multilayer_perceptron_gd/Train_and_valid_accuracy_per_epoch')
+
+
+# -------------------------------------------------------------------------------
+# 3. GENERAL
+# -------------------------------------------------------------------------------
+
+
+# Show plots
+
+plt.show()
