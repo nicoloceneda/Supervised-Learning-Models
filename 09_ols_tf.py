78f385c2c29d3f4168da6d9a8e5496105062469d
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-03-19 12:09:35 +0100

    Add files via upload

--- 09_ols_tf.py
+++ 09_ols_tf.py
@@ -37,13 +37,63 @@ X_train_norm = (X_train - np.mean(X_train)) / np.std(X_train)
 ds_train_orig = tf.data.Dataset.from_tensor_slices((tf.cast(X_train_norm, tf.float32), tf.cast(y_train, tf.float32)))
 
 
-# Define a model for linear regression
+# Instantiate a Model by subclassing the Model class
 
-class OrdinaryLeastSquares(tf.keras.Model):
+class MyModel(tf.keras.Model):
+
+    # Define the layers
 
     def __init__(self):
 
+        super(MyModel, self).__init__()
 
+        self.w = tf.Variable(0.0, name='weight')
+        self.b = tf.Variable(0.0, name='bias')
+
+    # Implement the forward pass
+
+    def call(self, x):
+
+        return self.w * x + self.b
+
+
+# Instantiate a subclassed model
+
+model = MyModel()
+
+
+# Build the model based on input shapes received
+
+model.build(input_shape=(None, 1))
+
+
+# Print the model summary
+
+model.summary()
+
+
+# Define the loss function
+
+def loss_fn(y_true, y_pred):
+
+    return tf.reduce_mean(tf.square(y_true - y_pred))
+
+
+# Testing the function:
+
+yt = tf.convert_to_tensor([1.0])
+yp = tf.convert_to_tensor([1.5])
+
+print(loss_fn(yt, yp))
+
+
+# Train the model
+
+def train(model, inputs, outputs, learning_rate):
+
+    with tf.GradientTape() as tape:
+
+        current_loss = loss_fn(model(inputs), outputs)
 
 # -------------------------------------------------------------------------------
 # 5. GENERAL
